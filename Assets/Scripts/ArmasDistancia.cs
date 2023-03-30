1669ef3c8c23b1d066cb37f3670492572df9a68f
Gabriel Riveros <gseclen99@gmail.com>
2023-03-30 12:09:42 -0500

    ArmasDistancia (update) , Axe(delete)

--- Assets/Scripts/ArmasDistancia.cs
+++ Assets/Scripts/ArmasDistancia.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 namespace weapons
 {
-    abstract class ArmasDistancia : Armas
+    class ArmasDistancia : Armas
     {
-        public float velocityAttack;
-        public float price;
+        public Projectile projectile;
 
-        public abstract float CalculateDamage();
+        public ArmasDistancia(string name, float damage, float velocity, float price, Projectile projectile)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.velocityAttack = velocity;
+            this.price = price;
+            this.projectile = projectile;
+        }
+        public override float CalculateDamage()
+        {
+            return projectile.damage + damage;
+        }
+        public override float CalculateDamageSegundo()
+        {
+            return projectile.damage + damage / velocityAttack;
+        }
     }
 }
